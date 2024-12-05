c8383ad510d5823970e23fc602077f198efc6f89
Ksireon <93395938+Ksireon@users.noreply.github.com>
2024-12-05 18:38:31 +0500

    new update

--- app/components/zapros1.jsx
+++ app/components/zapros1.jsx
@@ -22,7 +22,7 @@ export default function Zapros1() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:7777/openlessons', formData);
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/openlessons`, formData);
       setResponseMessage(response.data.message);
       setFormData({ firsName: '', number: '' });
     } catch (error) {
--- app/components/zapros2.jsx
+++ app/components/zapros2.jsx
@@ -23,7 +23,7 @@ export default function Zapros2() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:7777/otkritiyUrok', formData);
+            const response = await axios.post(`${process.env.REACT_APP_API_URL}/otkritiyUrok`, formData);
             setResponseMessage(response.data.message);
             setFormData({ firsName: '', number: '', urok: '' });
         } catch (error) {
