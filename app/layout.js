073a19b475f24fc43f8a310791138c9f393072ce
Ksireon <93395938+Ksireon@users.noreply.github.com>
2025-07-08 14:04:48 +0500

    new update

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import localFont from "next/font/local";
 import "./globals.scss";
 
@@ -20,7 +21,35 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <head>
+        {/* Meta Pixel noscript fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1422083055509092&ev=PageView&noscript=1"
+            alt="Meta Pixel"
+          />
+        </noscript>
+      </head>
+
+      <body>
+        {/* Meta Pixel Script */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1422083055509092');
+            fbq('track', 'PageView');
+          `}
+        </Script>
         {children}
       </body>
     </html>
